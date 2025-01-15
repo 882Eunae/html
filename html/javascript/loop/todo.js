35db2ffcd1e46e90a4b67b25a13ec2e12e91c1e9
882Eunae <pubao47@gmail.com>
2025-01-15 22:16:37 +0900

    0115homework

---
+++ html/javascript/loop/todo.js
@@ -0,0 +1,12 @@
+let scores=[];
+let max=0;
+for(let i=0; i<5; i++){
+
+    scores[i]=prompt("과목 성적을 입력하세요");
+
+    if(max<parseInt(scores[i])){
+        max=scores[i];
+    }
+}
+
+console.log(`최대값은 ${max} 입니다`);
